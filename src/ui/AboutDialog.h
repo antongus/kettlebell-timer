1b4827eb73c27d79850b7ba63d1ea73c458b0069
Anton Gusev <antongus@mail.ru>
2019-11-10 02:12:44 +0500

    add license information to source files

--- src/ui/AboutDialog.h
+++ src/ui/AboutDialog.h
@@ -3,7 +3,12 @@
 *
 *  About dialog class.
 *
-*  Author : Anton B. Gusev
+*  Copyright 2019 Anton B. Gusev
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version. See COPYING file for more details.
 **/
 
 #pragma once
